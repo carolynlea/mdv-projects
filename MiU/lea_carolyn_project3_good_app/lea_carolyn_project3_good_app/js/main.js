8a35584502af125c513148d302112b17ec3397d3
Carolyn Lea <mrscrl69@fullsail.edu>
2011-09-21 10:06:18 -0600

    trying to add some branding

--- MiU/lea_carolyn_project3_good_app/lea_carolyn_project3_good_app/js/main.js
+++ MiU/lea_carolyn_project3_good_app/lea_carolyn_project3_good_app/js/main.js
@@ -1,2 +1,6 @@
-var currentDate = new Date();  
-$("#mydate").datepicker("setDate",currentDate);
+//var currentDate = new Date();  
+//$("#mydate").datepicker("setDate",currentDate);
+
+
+
+$('#home').css('background', 'url(images/deepscratch.jpg)');
