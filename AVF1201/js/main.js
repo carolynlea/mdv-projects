36253e3d91748493b2a1bf1ebc90c63284dbc038
Carolyn Lea <mrscrl69@fullsail.edu>
2012-01-03 14:28:08 -0700

    imported last version of crud app

---
+++ AVF1201/js/main.js
@@ -0,0 +1,12 @@
+/*	CAROLYN LEA
+	PROJECT 1
+	ASD 1111
+	OCTOBER 27, 2011
+*/
+
+
+$('#home').css('background', 'url(images/deepscratch.jpg)');
+
+$('#launch').css('background', 'url(images/deepscratch.jpg)');
+
+
---
+++ AVF1201/js/site.js
@@ -0,0 +1,13 @@
+/*	CAROLYN LEA
+	PROJECT 1
+	ASD 1111
+	OCTOBER 27, 2011
+*/
+
+
+$(function(){
+	// site code
+});
+
+
+
